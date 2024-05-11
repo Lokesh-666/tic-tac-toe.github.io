eb76c252355ecd335d478a296d72fb27fe67ff3b
Lokesh-666 <lkblokesh12@gmail.com>
2024-05-11 12:52:05 +0530

    Modified game.cpp by adding more comments

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
-// C++ program to impliment tic-tac-toe game in cpp
+// C++ program to implement tic-tac-toe game in C++
+
 #include <iostream>
 using namespace std;
 
@@ -21,67 +22,73 @@ bool checkWin(char board[3][3], char player)
 	for (int i = 0; i < 3; i++) { 
 		if (board[i][0] == player && board[i][1] == player 
 			&& board[i][2] == player) 
-			return true; 
+			return true; // If any row has all same elements, return true
 		if (board[0][i] == player && board[1][i] == player 
 			&& board[2][i] == player) 
-			return true; 
+			return true; // If any column has all same elements, return true
 	} 
 	if (board[0][0] == player && board[1][1] == player 
 		&& board[2][2] == player) 
-		return true; 
+		return true; // If diagonal from top-left to bottom-right has all same elements, return true
 	if (board[0][2] == player && board[1][1] == player 
 		&& board[2][0] == player) 
-		return true; 
-	return false; 
+		return true; // If diagonal from top-right to bottom-left has all same elements, return true
+	return false; // Otherwise, return false
 } 
 
 // Main function
- int main(){
+int main(){
     // Initialise the board and the players
     char board[3][3] = {
         {' ', ' ', ' '},
         {' ', ' ', ' '},
-        {' ', ' ', ' '}};
-    char player = 'X';
+        {' ', ' ', ' '}
+    };
+    char player = 'X'; // Player X starts the game
     int row, col;
-    int turn; // declare turn here
+    int turn; // Variable to keep track of the turns
 
     cout << "Welcome to Tic-Tac-Toe!\n"; 
 
-    //Game loop
-    for(turn = 0; turn <9; turn++){
+    // Game loop
+    for(turn = 0; turn < 9; turn++){
         // Draw the board
         drawBoard(board);
 
         // Prompt for valid input
         while(true){
-            cout << "Player" << player << ", enter row (0-2) and column (0-2): "; 
+            cout << "Player " << player << ", enter row (0-2) and column (0-2): "; 
             cin >> row >> col ;
             // Check if the input is valid
-            if(board[row][col] == ' ') break;// Valid input, exit the loop
-            else {cout <<"Invalid input!"; continue; // loop continues}
-
+            if(board[row][col] == ' ') // If the selected cell is empty
+                break; // Valid input, exit the loop
+            else {
+                cout <<"Invalid input!\n"; 
+                continue; // Loop continues to prompt for input
+            }
         }
-    }
 
+        // Make the move
+        board[row][col] = player;
 
-    // Make the move
-    board[row][col] = player;
-    if(checkWin(board, player)){
+        // Check if the player wins
+        if(checkWin(board, player)){
             drawBoard(board);
-            cout<<"Player "<<player<<"Wins!\n";
-            break; // Exit the loop after someone wins!
+            cout<<"Player "<<player<<" wins!\n";
+            break; // Exit the loop after someone wins
+        }
+
+        // Switch players
+        player = (player == 'X') ? 'O' : 'X'; // Toggle between X and O
     }
-    // Switch players
-    player = (player== 'X') ? 'O' : 'X';
- }
-// End of the game
-drawBoard(board);
 
-// check for a draw
-if (turn == 9 && !checkWin(board, 'X') && !checkWin(board, 'O')){
-    cout << "It is a draw!\n";
+    // End of the game
+    drawBoard(board);
+
+    // Check for a draw
+    if (turn == 9 && !checkWin(board, 'X') && !checkWin(board, 'O')){
+        cout << "It's a draw!\n";
+    }
+
+    return 0;
 }
-
-return 0;
- }
